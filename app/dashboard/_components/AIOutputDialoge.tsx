125d8edb2e899ebe57d64424f73121a7f565a6e3
JunaidAhammedU <junaidahammed15@gmail.com>
2025-01-12 18:16:00 +0530

    fix: adjust spacing in AIOutputDialoge title for improved readability

--- app/dashboard/_components/AIOutputDialoge.tsx
+++ app/dashboard/_components/AIOutputDialoge.tsx
@@ -18,7 +18,10 @@ function AIOutputDialoge({
     <AlertDialog open={openOutputDialog}>
       <AlertDialogContent>
         <AlertDialogHeader>
-          <AlertDialogTitle className="text-center text-lg"> Here is your result.</AlertDialogTitle>
+          <AlertDialogTitle className="text-center text-lg">
+            {" "}
+            Here is your result.
+          </AlertDialogTitle>
           <BeforeAfter beforeImage={orgImage} afterImage={aiImageUrl} />
           <Button onClick={() => closeOutputDialog(false)}>Close</Button>
         </AlertDialogHeader>
